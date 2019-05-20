b458a35b9cf9b73ad2535ada8987dba70e827257
Tobias Kruse <tobias.kruse@mailbox.org>
2019-05-20 10:56:08 +0200

    fixed docker-compose with api and web containers

--- server/api/src/bootstrap.py
+++ server/api/src/bootstrap.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://api:api-server-password@127.0.0.1:3307/api'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://api:api-server-password@db:3306/api'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
