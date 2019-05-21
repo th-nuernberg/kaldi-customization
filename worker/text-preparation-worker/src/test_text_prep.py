6429606b9fdca21d61a858b685ea06bb571d5904
Dino Pehratovic <dino.pehratovic@outlook.de>
2019-05-21 16:31:55 +0200

    Updated text-prep-worker. It is now possible to communicate with redis server

---
+++ worker/text-preparation-worker/src/test_text_prep.py
@@ -0,0 +1,24 @@
+# Testing text-files
+# process_file(working_dir + "/test-files/txt/kafka.txt")
+# process_file(working_dir + "/test-files/txt/text_generator.txt")
+
+# Testing HTML-files
+# process_file(working_dir + "/test-files/html/hein.html")
+# process_file(working_dir + "/test-files/html/hochschule.html")
+# process_file(working_dir + "/test-files/html/gamestar.html")
+
+# Testing PDF-files
+# process_file(working_dir + "/test-files/pdf/kafka.pdf")
+# process_file(working_dir + "/test-files/pdf/text_generator.pdf")
+
+# Testing Word-files
+# process_file(working_dir + "/test-files/word/kafka.docx")
+# process_file(working_dir + "/test-files/word/text_generator.docx")
+
+# Testing PNG-files
+# process_file(working_dir + "/test-files/png/bild.png")
+# process_file(working_dir + "/test-files/png/dokument.png")
+
+# Testing JPG-files
+# process_file(working_dir + "/test-files/jpg/bild.jpg")
+# process_file(working_dir + "/test-files/jpg/dokument.jpg")
--- worker/text-preparation-worker/src/text_preparation.py
+++ worker/text-preparation-worker/src/text_preparation.py
@@ -1,8 +1,10 @@
 # -*- encoding: utf-8 -*-
 import os
 import re
+import redis
 import PyPDF2 
 import docx
+import json
 from bs4 import BeautifulSoup, Comment
 try:
     from PIL import Image
@@ -57,24 +59,25 @@ def create_unique_list(word_list):
 '''
     The following 5 functions are different kinds of parsers, depending on the given file type.
 '''
-def pdf_parser(binary_file_handle):
-    pdfReader = PyPDF2.PdfFileReader(binary_file_handle)
+def pdf_parser(file_path):
+    with open(file_path, "rb") as file_handler:
+        pdfReader = PyPDF2.PdfFileReader(file_handler)
 
-    # These variables are needed in order to parse the whole PDF-file
-    num_pages = pdfReader.numPages
-    count = 0
-    text = ""
+        # These variables are needed in order to parse the whole PDF-file
+        num_pages = pdfReader.numPages
+        count = 0
+        text = ""
 
-    #The while loop will read each page
-    #Important note: Some lines of the PDF-files are read incorect. 
-    #TODO: Search for different PDF reader libraries and test whether they achieve better results
-    while count < num_pages:
-        pageObj = pdfReader.getPage(count)
-        count +=1
-        text += pageObj.extractText()
+        #The while loop will read each page
+        #Important note: Some lines of the PDF-files are read incorect. 
+        #TODO: Search for different PDF reader libraries and test whether they achieve better results
+        while count < num_pages:
+            pageObj = pdfReader.getPage(count)
+            count +=1
+            text += pageObj.extractText()
 
-    word_list = retrieve_all_words(text)
-    unique_list = create_unique_list(word_list)
+        word_list = retrieve_all_words(text)
+        unique_list = create_unique_list(word_list)
 
     return unique_list
 
@@ -99,36 +102,38 @@ def word_parser(file_path):
     return unique_list
 
 
-def html_parser(file_handler):
-    # Blacklisted tags which will be ignored and extracted from the HTML-files
-    blacklist = ["script", "style"]
+def html_parser(file_path):
+    with open(file_path, "r", encoding="ISO-8859-1") as file_handler:
+        # Blacklisted tags which will be ignored and extracted from the HTML-files
+        blacklist = ["script", "style"]
+        soup = BeautifulSoup(file_handler.read(), features="html.parser")
+        # Strips all blacklisted HTML-tags
 
-    soup = BeautifulSoup(file_handler, features="html.parser")
+        for tag in soup.findAll():
+            if tag.name.lower() in blacklist:
+                tag.extract()
 
-    # Strips all blacklisted HTML-tags
-    for tag in soup.findAll():
-        if tag.name.lower() in blacklist:
-            tag.extract()
+        # Retrieves all comments from the HTML-file and removes them
 
-    # Retrieves all comments from the HTML-file and removes them
-    comments = soup.findAll(text=lambda text:isinstance(text, Comment))
-    for comment in comments:
-        comment.extract()
+        comments = soup.findAll(text=lambda text:isinstance(text, Comment))
+        for comment in comments:
+            comment.extract()
 
-    # Retrieves the visible text of the web page
-    webpage_text = soup.body.getText().encode("utf8")
-        
-    word_list = retrieve_all_words(webpage_text)
-    unique_list = create_unique_list(word_list)
+        # Retrieves the visible text of the web page
+        webpage_text = soup.body.getText()
+
+        word_list = retrieve_all_words(webpage_text)
+        unique_list = create_unique_list(word_list)
     
     return unique_list
 
 
-def text_parser(file_handler):
-    text = file_handler.read()
+def text_parser(file_path):    
+    with open(file_path, "r") as file_handler:
+        text = file_handler.read()
 
-    all_words = retrieve_all_words(text)
-    unique_words = create_unique_list(all_words)
+        all_words = retrieve_all_words(text)
+        unique_words = create_unique_list(all_words)
 
     return unique_words
 
@@ -150,55 +155,58 @@ def ocr_parser(file_path):
     All files which need to be processed are saved within:
         /text-preparation/in/<project_id>/<model_id>/<id>
 '''
-def process_file(file_path):
-
-    try:
-        normal_file_handle = open(file_path, "r")
-        binary_file_handle = open(file_path, "rb")
-    except:
-        raise OSError("File not found within the given directory!")
-
+def process_file(file_path, file_type):
+    '''
+        try:
+            normal_file_handle = open(file_path, "r", encoding="ISO-8859-1")
+            binary_file_handle = open(file_path, "rb")
+        except:
+            return (False, "File not found within given directory")
+    '''
     parsed_text = []
 
-    if file_path.endswith(".pdf"):
-        parsed_text = pdf_parser(binary_file_handle)
-    elif file_path.endswith(".docx"):
-        parsed_text = word_parser(file_path)
-    elif file_path.endswith(".html"):
-        parsed_text = html_parser(normal_file_handle)
-    elif file_path.endswith(".txt"):
-        parsed_text = text_parser(normal_file_handle)
-    elif file_path.endswith(".png") or file_path.endswith(".jpg"):
-        parsed_text = ocr_parser(file_path)
-    else:
-        print("Given file is not supported. Finishing processing")
-        exit(1)
+    try:
+        if file_type == "pdf":
+            parsed_text = pdf_parser(file_path)
+        elif file_type == "docx":
+            parsed_text = word_parser(file_path)
+        elif file_type == "html":
+            parsed_text = html_parser(file_path)
+        elif file_type == "txt":
+            parsed_text = text_parser(file_path)
+        elif file_type == "png" or file_type == "jpg":
+            parsed_text = ocr_parser(file_path)
+        else:        
+            return (False, "Given file type is not supported. Finishing processing")
+    except Exception as e:
+        print(e)
+        return (False, "Failed to parse given file")
 
-    save_textfile(parsed_text)    
+
+    try:
+        save_textfile(parsed_text)
+    except:
+        return (False, "Failed to save word list")
+    return (True, "Processing of data has finished successfully")
 
 
-working_dir = os.getcwd()
-# Testing text-files
-# process_file(working_dir + "/test-files/txt/kafka.txt")
-# process_file(working_dir + "/test-files/txt/text_generator.txt")
+def infinite_loop():
+    working_dir = os.getcwd()
 
-# Testing HTML-files
-# process_file(working_dir + "/test-files/html/hein.html")
-# process_file(working_dir + "/test-files/html/hochschule.html")
-# process_file(working_dir + "/test-files/html/gamestar.html")
+    conn = redis.Redis(host='redis', port=6379, db=0, password="kalditproject")
+    pubsub = conn.pubsub()
+    while True:
 
-# Testing PDF-files
-# process_file(working_dir + "/test-files/pdf/kafka.pdf")
-# process_file(working_dir + "/test-files/pdf/text_generator.pdf")
+        data = conn.blpop('Text-Prep-Queue', 1)        
+        if data:
+            print("Received the following task from Text-Prep-Queue: ")
+            print(data)
+            json_data = json.loads(data[1])
+            print("Starting to process received data")
+            return_value = process_file("/text_prep_worker/in/" + json_data["text"], json_data["type"])
+            print(return_value[1])
 
-# Testing Word-files
-# process_file(working_dir + "/test-files/word/kafka.docx")
-# process_file(working_dir + "/test-files/word/text_generator.docx")
-
-# Testing PNG-files
-# process_file(working_dir + "/test-files/png/bild.png")
-# process_file(working_dir + "/test-files/png/dokument.png")
-
-# Testing JPG-files
-# process_file(working_dir + "/test-files/jpg/bild.jpg")
-# process_file(working_dir + "/test-files/jpg/dokument.jpg")
+if __name__ == "__main__":
+    print("Text-Prep-Worker is running")
+    infinite_loop()
+    print("text-Prep-Worker stops running")
