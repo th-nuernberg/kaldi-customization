99175d15ac6761ec54d21a2272d70e95bd3dd339
Max Körber <max.koerber@gmx.de>
2019-06-10 14:29:01 +0200

    added method to serialize db object

--- server/api/src/db/__init__.py
+++ server/api/src/db/__init__.py
@@ -17,3 +17,9 @@ FOR EACH ROW BEGIN
        WHERE model_id  = NEW.model_id
     );
 END''')
+
+from sqlalchemy import inspect
+def object_as_dict(obj):
+    return {c.key: getattr(obj, c.key)
+            for c in inspect(obj).mapper.column_attrs}
+            
