09d156333a70c67175d6017d7c55e97a4459928e
Dino Pehratovic <dino.pehratovic@outlook.de>
2019-07-05 15:00:44 +0200

    Implemented corpus generation. MinIO down- and upload were implemented. Restructured the scripts

---
+++ worker/text-preparation-worker/src/minio_communication.py
@@ -0,0 +1,22 @@
+import minio
+from minio import ResponseError
+
+
+def download_from_bucket(minio_client, bucket, filename, target_path):
+    try:
+        print(minio_client.fget_object(bucket, filename, target_path))
+
+    except ResponseError as err:
+        print(err)
+        return (False, err)
+    return (True, "")
+
+
+def upload_to_bucket(minio_client, bucket, object_name, file_path):
+    # Put an object 'myobject' with contents from '/tmp/otherobject'
+    # upon success prints the etag identifier computed by server.
+    try:
+        print(minio_client.fput_object(bucket, object_name, file_path))
+    except ResponseError as err:
+        print(err)
+        return (False, err)
--- worker/text-preparation-worker/src/text_preparation.py
+++ worker/text-preparation-worker/src/text_preparation.py
@@ -1,26 +1,13 @@
 # -*- encoding: utf-8 -*-
-import docx
 import json
 import os
-import PyPDF2
-import re
 import redis
-import tempfile
-from bs4 import BeautifulSoup, Comment
+from minio import Minio
+from minio import ResponseError
 
-try:
-    from PIL import Image
-except ImportError:
-    import Image
-import pytesseract
-
-from pdf2image import convert_from_path, convert_from_bytes
-from pdf2image.exceptions import (
-    PDFInfoNotInstalledError,
-    PDFPageCountError,
-    PDFSyntaxError
-)
-
+from file_parser import pdf_parser, html_parser, word_parser
+from file_parser import text_parser, ocr_parser, generate_corpus
+from minio_communication import download_from_bucket, upload_to_bucket
 
 def report_status_to_API(queue_status, conn, filename=None, message=None):
     '''
@@ -28,12 +15,12 @@ def report_status_to_API(queue_status, conn, filename=None, message=None):
     Possible updates for the status queue are:
         - InProgress
         - Success
-        - Failure       
+        - Failure
     As soon as the Text-Preparation-Worker receives a new task, its entry
     within the Status-Queue is updated to: In-Progress.
     Depending on the processing result, the status of the task can change into:
         - Success
-        - Failure        
+        - Failure
     '''
     if queue_status == 11 and not message:
         message = "Task in progress"
@@ -43,139 +30,27 @@ def report_status_to_API(queue_status, conn, filename=None, message=None):
         message = "Task finished successfully"
 
     conn.publish('Status-Queue', json.dumps({
-    "type": "text-prep",
-    "text": filename,
-    "status": queue_status,
-    "msg": message
-    }))
+                 "type": "text-prep",
+                 "text": filename,
+                 "status": queue_status,
+                 "msg": message
+                 }))
+
 
 def save_textfile(text_list, filename):
     '''
-    This function saves the unique word list into the file system as a txt-file.
+    This function saves the unique word list into the file system as a txt-file
     The following directory will be used to save the unique world list:
         /text-preparation/out/<filename>.txt
     '''
 
-    f = open("/text_prep_worker/out/" + filename, "w")
+    file_handler = open("/text_prep_worker/out/" + filename, "w")
     for sentence in text_list:
-        f.write(sentence + "\n")
-    f.close()
+        file_handler.write(sentence + "\n")
+    file_handler.close()
 
 
-def retrieve_all_words(text):
-    remove_newline_after_minus = re.sub("[--–—−]\n[a-z]+", "", text)
-    regex = r"[^a-zA-ZäöüÄÖÜß]+"
-    all_words = re.split(regex, remove_newline_after_minus)
-    return all_words
-
-
-def create_unique_list(word_list):
-    unique_word_list = sorted(list(set(word_list)))
-    return unique_word_list
-
-
-def pdf_parser(file_path):
-    # Converts all pages of the PDF-file into PNG-files
-    print("Starting to transform the received PDF-file into PNG-files")
-    with tempfile.TemporaryDirectory() as path:
-        images = convert_from_path(file_path,
-                                output_folder=path,
-                                dpi=300, fmt=".jpg")
-
-    print("Finished transforming the PDF-file into image-files")
-
-    # Iterates through all images and retrieves the word list
-    print("Starting to process the image-files with the OCR-scanner")
-    complete_word_list = []
-    for image in images:
-        print("Starting to process : " + str(image))
-        text = pytesseract.image_to_string(image, lang="deu")
-        print("Finished retrieving text from " + str(image))
-
-        print("Starting to create the word_list for the text")
-        word_list = retrieve_all_words(text)
-        print("Finished finished processing of " + str(image))
-
-        for word in word_list:
-            complete_word_list.append(word)
-        print("--------------------------------------")
-
-    # After retrieving all words, the unique word list is created
-    print("Creating unique word list")
-    unique_word_list = create_unique_list(complete_word_list)
-
-    return unique_word_list
-
-
-def word_parser(file_path):
-    # Opens word file properly
-    word_doc = docx.Document(file_path)
-    
-    # Extract text from file into a list
-    fullText = []    
-    for para in word_doc.paragraphs:
-        fullText.append(para.text)
-    
-    # Create the word list and the unique list
-    word_list = []
-    for paragraph in fullText:
-        words = retrieve_all_words(paragraph)
-        for word in words:
-            word_list.append(word)
-    unique_word_list = create_unique_list(word_list)
-
-    return unique_word_list
-
-
-def html_parser(file_path):
-    with open(file_path, "r", encoding="ISO-8859-1") as file_handler:
-        # Blacklisted tags which will be ignored and extracted from the HTML-files
-        blacklist = ["script", "style"]
-        soup = BeautifulSoup(file_handler.read(), features="html.parser")
-        # Strips all blacklisted HTML-tags
-
-        for tag in soup.findAll():
-            if tag.name.lower() in blacklist:
-                tag.extract()
-
-        # Retrieves all comments from the HTML-file and removes them
-
-        comments = soup.findAll(text=lambda text:isinstance(text, Comment))
-        for comment in comments:
-            comment.extract()
-
-        # Retrieves the visible text of the web page
-        webpage_text = soup.body.getText()
-
-        word_list = retrieve_all_words(webpage_text)
-        unique_word_list = create_unique_list(word_list)
-    
-    return unique_word_list
-
-
-def text_parser(file_path):    
-    with open(file_path, "r") as file_handler:
-        text = file_handler.read()
-
-        all_words = retrieve_all_words(text)
-        unique_word_list = create_unique_list(all_words)
-
-    return unique_word_list
-
-
-def ocr_parser(file_path):
-    # Open image and extract text into pdf
-    text = pytesseract.image_to_string(Image.open(file_path), lang="deu")
-
-    # Extracted text is a string. Therefore, it is possible to retrieve all words
-    # and after that create the unique_word_list
-    word_list = retrieve_all_words(text)
-    unique_word_list = create_unique_list(word_list)
-
-    return unique_word_list
-
-
-def process_file(file_type, filename):
+def process_file(file_type, filename, minio_client):
     '''
     This function is called, in order to open the received filename of the API.
     All files which need to be processed are saved within:
@@ -188,37 +63,74 @@ def process_file(file_type, filename):
         - PNG or JPG
     '''
     parsed_text = []
-    file_path = "/text_prep_worker/in/" + filename
+    # Checks whether the requested bucket exists
+    try:
+        print(minio_client.bucket_exists("texts-in"))
+    except ResponseError as err:
+        print(err)
+        return (False, err)
 
+    # Get a full object and prints the original object stat information.
+    download_result = download_from_bucket(minio_client, 'texts-in', filename, 'text_prep_worker/in/')
+    if not download_result[0]:
+        return download_result
+
+    # Starts to process the downloaded file
+    file_path = "/text_prep_worker/in/" + filename
+    full_text = ""
     try:
         if file_type == "pdf":
-            parsed_text = pdf_parser(file_path)
+            parsed_text, full_text = pdf_parser(file_path)
         elif file_type == "docx":
-            parsed_text = word_parser(file_path)
+            parsed_text, full_text = word_parser(file_path)
         elif file_type == "html":
-            parsed_text = html_parser(file_path)
+            parsed_text, full_text = html_parser(file_path)
         elif file_type == "txt":
-            parsed_text = text_parser(file_path)
+            parsed_text, full_text = text_parser(file_path)
         elif file_type == "png" or file_type == "jpg":
-            parsed_text = ocr_parser(file_path)
-        else:        
+            parsed_text, full_text = ocr_parser(file_path)            
+        else:
             return (False, "Given file type is not supported. Finishing processing")
+
+        # Generates the corpus
+        corpus = generate_corpus(full_text)
+
     except Exception as e:
         print(e)
         return (False, "Failed to parse given file")
 
     try:
+        corpus_name = filename + "_corpus"
         save_textfile(parsed_text, filename)
+        save_textfile(corpus, corpus_name)
     except:
         return (False, "Failed to save word list")
+
+    # Saves the created unique word list
+    file_path = "/text_prep_worker/out/" + filename
+    file_upload_result = upload_to_bucket(minio_client, "texts-out", filename, file_path)
+    if not file_upload_result[0]:
+        return file_upload_result
+
+    # Saves the created corpus
+    corpus_path = "/text_prep_worker/out/" + corpus_name
+    corpus_upload_result = upload_to_bucket(minio_client, "texts-out", corpus_name, corpus_path)
+    if not corpus_upload_result[0]:
+        return corpus_upload_result
+
     return (True, "Processing of data has finished successfully")
 
 
 def infinite_loop():
     conn = redis.Redis(host='redis', port=6379, db=0, password="kalditproject")
+    minio_client = Minio('minio:9000',
+                         access_key='AKIAIOSFODNN7EXAMPLE',
+                         secret_key='wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
+                         secure=False)
+
     while True:
 
-        data = conn.blpop('Text-Prep-Queue', 1)      
+        data = conn.blpop('Text-Prep-Queue', 1)
         if data:
             print("Received the following task from Text-Prep-Queue: ")
             print(data)
@@ -228,7 +140,7 @@ def infinite_loop():
                 if "text" in json_data and "type" in json_data:
                     report_status_to_API(queue_status=11, conn=conn, filename=json_data["text"])
 
-                    return_value = process_file(json_data["type"], json_data["text"])    
+                    return_value = process_file(json_data["type"], json_data["text"], minio_client)
 
                     # If the task was successfully processed, the if-statement is executed
                     # Otherwise, the status queue is updated to: failure
@@ -236,7 +148,7 @@ def infinite_loop():
                         report_status_to_API(queue_status=200, conn=conn, filename=json_data["text"])
                     else:
                         report_status_to_API(queue_status=12, conn=conn, filename=json_data["text"], message=return_value[1])
-                    
+
                     print(return_value[1])
                 else:
                     # Received parameters are wrong --> Update status queue and set task processing to: failure
@@ -245,13 +157,13 @@ def infinite_loop():
                     elif "type" not in json_data and "text" in json_data:
                         report_status_to_API(queue_status=12, conn=conn, filename="failure", message="type key is missing or misspelled within the JSON.")
                     else:
-                        report_status_to_API(queue_status=12, conn=conn, filename="failure", message="Both keys are not correct")                                        
+                        report_status_to_API(queue_status=12, conn=conn, filename="failure", message="Both keys are not correct")
             except:
                 # Received parameters are wrong --> Update status queue and set task processing to: failure
                 report_status_to_API(queue_status=12, conn=conn, filename="failure", message="Data is not valid JSON. Processing cancelled")
-            
+
 
 if __name__ == "__main__":
     print("Text-Prep-Worker is running")
-    infinite_loop()    
-    print("text-Prep-Worker stops running")
+    infinite_loop()
+    print("Text-Prep-Worker stops running")
