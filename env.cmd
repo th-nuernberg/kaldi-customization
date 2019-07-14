634702e203c2499f510752509446631c4410ebf2
Tobias Kruse <tobias.kruse@mailbox.org>
2019-07-14 13:10:11 +0200

    fixed docker setup

--- env.cmd
+++ env.cmd
@@ -4,4 +4,3 @@ REM Adds the environments variables from file ./config/.env
 for /f "tokens=*" %%a in (%~dp0/config/.env) do (
     for /f "tokens=1,2 delims==" %%b in ("%%a") do set %%b=%%c
 )
-pause
--- worker/kaldi-worker/worker.py
+++ worker/kaldi-worker/worker.py
@@ -15,7 +15,6 @@ if __name__ == "__main__":
 
         for task in tasks.listen():
             task = KaldiTask(**task)
-            print(task)
 
             os.makedirs(workspace_path)
 
