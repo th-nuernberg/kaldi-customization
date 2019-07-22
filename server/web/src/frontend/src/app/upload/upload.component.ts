c213794bd03e16a307ec7beb7ef90e312aeef337
Florian Richter <richter.r.florian@gmail.com>
2019-07-22 08:07:54 +0200

    minor refactor and added placeholders for future api merge/usage

--- server/web/src/frontend/src/app/upload/upload.component.ts
+++ server/web/src/frontend/src/app/upload/upload.component.ts
@@ -40,7 +40,9 @@ export class UploadComponent implements OnInit {
   ngOnInit() {
   }
 
-  handleSelection(event, file) {
+  // enables single selection for current panel list
+  handleSelection(event) {
+    // TODO: show and clean preview content of selected file
     if (event.option.selected) {
       event.source.deselectAll();
       event.option._setSelected(true);
@@ -71,6 +73,7 @@ export class UploadComponent implements OnInit {
 
   // copies selected history elements to current panel
   copy() {
+    // TODO: do not copy text files only unique_word_list and corpuses
     this.historySelection.selected.forEach(file => {
       if(!this.uploadedFiles.includes( { name:file.name, selected:true }))
       {
@@ -85,22 +88,39 @@ export class UploadComponent implements OnInit {
 
   // removes selected history elements
   remove(files:any) {
+    // TODO: find more generic solution
     files.selectedOptions.selected.forEach(file => this.uploadedFiles.splice(this.uploadedFiles.indexOf(file)));
   }
 
   // toggles remove button (disabled if nothing is selected)
   isRemoveDisabled(files:any) {
+    // TODO find more generic solution
     return files.selectedOptions.selected.length === 0;
   }
 
   // toggles save button (dummy disabled, should be enabled if text changed)
   shouldSave() {
+    // TODO toggles, when user changed the content of the selected file
     return true;
   }
 
   // uploads file and show preview
   loadFile(file:HTMLInputElement, list:any) {
 
+    // TODO: API - TPW Results as uploaded files in current file list
+    this.sendFileToAPI();
+    let api_result = this.getResultFromAPI();
+    this.dummyShowUploadedFile(file, list)
+
+    // loads content of uploaded file into preview
+    this.showPreview(file)
+    this.show = false;
+  }
+
+  sendFileToAPI() {}
+  getResultFromAPI() {}
+
+  dummyShowUploadedFile(file, list) {
     let fileName = file.files[0].name;
     // add uploaded file to current file list
     this.uploadedFiles.push({ name:fileName, selected:true });
@@ -108,10 +128,6 @@ export class UploadComponent implements OnInit {
     // selects element in current panel
     this.currentFiles = list.selectedOptions.selected.map(item => item.value);
     this.currentFiles.push(fileName);
-
-    // loads content of uploaded file into preview
-    this.showPreview(file)
-    this.show = false;
   }
 
   // toggles start and verify button in current panel
