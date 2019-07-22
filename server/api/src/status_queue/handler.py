81ef08d91324220579e6ec89deedf7627d12e8e3
Max Körber <max.koerber@gmx.de>
2019-07-22 22:08:44 +0200

    fixed status queue and improved logging

--- server/api/src/status_queue/handler.py
+++ server/api/src/status_queue/handler.py
@@ -2,38 +2,42 @@ import json
 import threading
 
 from .text_prep import handle_text_prep_status
+from config import redis_client
+from redis_config import redis_queues
 
 
 def handle_statue_queue(status_queue, db):
     '''
     Listens to STATUS_QUEUE and handle the messages.
     '''
+    status_queue_pubsub = redis_client.pubsub(ignore_subscribe_messages=True)
+    status_queue_pubsub.subscribe(status_queue)
+    print("[Status] Listen to {}".format(status_queue))
 
-    for message in status_queue.listen():
+    for message in status_queue_pubsub.listen():
         try:
-            print("Status: new pubsub message:")
-            print(message)
+            print("[Status] New pubsub message: " + str(message))
             if message['type'] == 'message':
                 try:
                     msg_data = json.loads(message['data'])
                 except ValueError as e:
-                    print(e)
+                    print("[Status] Error at processing message: " + str(e))
                     continue
                 
                 if msg_data and "type" in msg_data and "text" in msg_data and "status" in msg_data:
                     if msg_data['type'] == 'text-prep':
-                        print("Status: handle text prep status...")
+                        print("[Status] handle text prep status...")
                         handle_text_prep_status(msg_data, db)
-                        print("Status: ...handled text prep status")
+                        print("[Status] ...handled text prep status")
                     else:
-                        print('unknown type in status queue!')
+                        print("[Status] unknown type \"{}\" in status queue!".format(msg_data['type']))
         except Exception as e:
-            print("Exception at status queue: {}".format(type(e).__name__))
-            print(e.__str__())
+            print("[Status] Exception at status queue: {}".format(type(e).__name__))
+            print("[Status] Further information: " + e.__str__())
 
 
-def start_status_queue_handler(status_queue, db):
-    thread = threading.Thread(target=handle_statue_queue, name="Redis-Handler", args=(status_queue, db))
+def start_status_queue_handler(db):
+    thread = threading.Thread(target=handle_statue_queue, name="Redis-Handler", args=(redis_queues["STATUS_QUEUE"], db))
     thread.start()
 
     return thread
--- server/api/src/status_queue/text_prep.py
+++ server/api/src/status_queue/text_prep.py
@@ -6,23 +6,21 @@ def handle_text_prep_status(msg_data, db):
     Handle a status message from a text preparation worker.
     '''
     if msg_data['text'] == 'failure':
-        print('Status: ERROR: Failure at Text-Prep-Worker: ')
-        if "msg" in msg_data:
-            print("Status: ERROR: Error message: " + msg_data['msg'])
+        error_message = str(msg_data['msg']) if "msg" in msg_data else "-"
+        print('[Status] ERROR: Failure at Text-Prep-Worker: {}'.format(error_message))
     else:
         this_resource = Resource.query.filter_by(name=msg_data['text']).first()
         if this_resource is not None:
-            print('Status: found resource in db: ' + this_resource.__repr__())
+            print('[Status] found resource in db: ' + this_resource.__repr__())
             try:
                 resource_status = FileStateEnum(msg_data['status'])
-                print("Status: resource status: " + FileStateEnum.status_to_string(resource_status))
+                print("[Status] resource status: " + FileStateEnum.status_to_string(resource_status))
             except ValueError as e:
-                print("Status: WARN: status is not valid!")
-                print(e)
+                print("[Status] WARN: status is not valid! " + str(e))
                 resource_status = FileStateEnum.TextPreparation_Failure
             
             this_resource.status = resource_status
-            print('Status: after update: ' + this_resource.__repr__())
+            print('[Status] after update: ' + this_resource.__repr__())
             db.session.add(this_resource)
 
             if resource_status == FileStateEnum.Success:
@@ -33,13 +31,13 @@ def handle_text_prep_status(msg_data, db):
                                             name=this_resource.name,
                                             resource_type=FileTypeEnum.unique_word_list,
                                             status=FileStateEnum.G2P_Ready)
-                    print('Status: added db entry for g2p resource file: ' + db_resource.__repr__())
+                    print('[Status] added db entry for g2p resource file: ' + db_resource.__repr__())
                     db.session.add(db_resource)
                 except Exception as e:
-                    print("Status: ERROR: Error at adding entry for g2p!")
+                    print("[Status] ERROR: Error at adding entry for g2p!")
                     raise e
 
             db.session.commit()
             db.session.close()
         else:
-            print('Status: WARN: did not found resource in db: ' + msg_data['text'] + '!')
+            print('[Status] WARN: did not found resource in db: ' + msg_data['text'] + '!')
