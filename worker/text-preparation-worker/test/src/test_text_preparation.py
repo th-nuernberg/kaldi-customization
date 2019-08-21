8f972b96b21db9f8b0815e59e3f58ddac6d722a1
Dino Pehratovic <dino.pehratovic@outlook.de>
2019-08-21 22:28:17 +0200

    Updated test worker of the text-preparation-worker

--- worker/text-preparation-worker/test/src/test_text_preparation.py
+++ worker/text-preparation-worker/test/src/test_text_preparation.py
@@ -18,64 +18,76 @@ def test_text_prep(redis_client, minio_client):
 
 
     # Step 2: Upload needed files into the texts-in bucket
-    upload_to_bucket(minio_client, minio_buckets["RESOURCE_BUCKET"], "1/kafkatxt", "test-files/txt/kafkatxt")
-    upload_to_bucket(minio_client, minio_buckets["RESOURCE_BUCKET"], "2/kafkapdf", "test-files/pdf/kafkapdf")
-    upload_to_bucket(minio_client, minio_buckets["RESOURCE_BUCKET"], "3/kafkadocx", "test-files/word/kafkadocx")
-    upload_to_bucket(minio_client, minio_buckets["RESOURCE_BUCKET"], "4/gamestarhtml", "test-files/html/gamestarhtml")
-    upload_to_bucket(minio_client, minio_buckets["RESOURCE_BUCKET"], "5/dokumentjpg", "test-files/jpg/dokumentjpg")
-    upload_to_bucket(minio_client, minio_buckets["RESOURCE_BUCKET"], "6/dokumentpng", "test-files/png/dokumentpng")
+    upload_to_bucket(minio_client, minio_buckets["RESOURCE_BUCKET"], "1/source", "test-files/txt/kafkatxt")
+    upload_to_bucket(minio_client, minio_buckets["RESOURCE_BUCKET"], "2/source", "test-files/pdf/kafkapdf")
+    upload_to_bucket(minio_client, minio_buckets["RESOURCE_BUCKET"], "3/source", "test-files/word/kafkadocx")
+    upload_to_bucket(minio_client, minio_buckets["RESOURCE_BUCKET"], "4/source", "test-files/html/gamestarhtml")
+    upload_to_bucket(minio_client, minio_buckets["RESOURCE_BUCKET"], "5/source", "test-files/jpg/dokumentjpg")
+    upload_to_bucket(minio_client, minio_buckets["RESOURCE_BUCKET"], "6/source", "test-files/png/dokumentpng")
     # Step 3: Create redis tasks for the following file types:
     #      3.1: txt-file
     text_file_task = {
-        "resource_id": "1",
-        "filename" : "kafkatxt",
-        "type" : "txt"
+        "resource_uuid": "1",
+        "file_type" : "txt"
     }
     #      3.2: PDF-file
     pdf_file_task = {
-        "resource_id": "2",
-        "filename" : "kafkapdf",
-        "type" : "pdf"
+        "resource_uuid": "2",
+        "file_type" : "pdf"
     }
     #      3.3: docx-file
     docx_file_task = {
-        "resource_id": "3",
-        "filename" : "kafkadocx",
-        "type" : "docx"
+        "resource_uuid": "3",
+        "file_type" : "docx"
     }
-    #      3.4: jpg image
-    jpg_file_task = {
-        "resource_id": "5",
-        "filename" : "dokumentjpg",
-        "type" : "jpg"
-    }
-    #      3.5: png image
-    png_file_task = {
-        "resource_id": "6",
-        "filename" : "dokumentpng",
-        "type" : "png"
-    }
-    #      3.6: html-file
+    #      3.4: html-file
     html_file_task = {
-        "resource_id": "4",
-        "filename" : "gamestarhtml",
-        "type" : "html"
+        "resource_uuid": "4",
+        "file_type" : "html"
+    }
+    #      3.5: jpg image
+    jpg_file_task = {
+        "resource_uuid": "5",
+        "file_type" : "jpg"
+    }
+    #      3.6: png image
+    png_file_task = {
+        "resource_uuid": "6",
+        "file_type" : "png"
     }
 
     # Step 4: Subscribe to the status-queue channel
-    pubsub = redis_client.pubsub()
+    pubsub = redis_client.pubsub(ignore_subscribe_messages=True)
     pubsub.subscribe('Status-Queue')
     
     # Step 5: Send all tasks for text-prep-worker into the queue
     redis_client.rpush('Text-Prep-Queue', json.dumps(text_file_task))
     redis_client.rpush('Text-Prep-Queue', json.dumps(pdf_file_task))
     redis_client.rpush('Text-Prep-Queue', json.dumps(docx_file_task))
+    redis_client.rpush('Text-Prep-Queue', json.dumps(html_file_task))
     redis_client.rpush('Text-Prep-Queue', json.dumps(jpg_file_task))
     redis_client.rpush('Text-Prep-Queue', json.dumps(png_file_task))
-    redis_client.rpush('Text-Prep-Queue', json.dumps(html_file_task))
 
     # Step 6: Listen to all incoming Status-queue messages
-    for msg in pubsub.listen(): print(msg)
+    count = 0
+    for msg in pubsub.listen(): 
+        print("Received the following message:")        
+        print(msg)
+        data_part = json.loads(msg['data'])
+        if data_part['id'] == 10:
+            pass
+        elif data_part['id'] == 200:
+            count += 1
+        else:
+            print("At least one task did NOT finish SUCCESSFULLY")
+            print("Exiting with code 999")
+            exit(999)
+        if count == 6:
+            print("###############################")
+            print("All tasks finished SUCCESSFULLY")
+            print("Exiting with code 0")
+            print("###############################")
+    exit(0)
 
 
 
