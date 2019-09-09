22c970736ad6f69828277d7e9ee13c2a2a3afce3
Florian Richter <richter.r.florian@gmail.com>
2019-09-09 18:57:20 +0200

    adapted decode page due to new functionality (dummy buttons and behaviors)

--- server/web/src/frontend/src/app/upload/decoding/decoding.upload.component.ts
+++ server/web/src/frontend/src/app/upload/decoding/decoding.upload.component.ts
@@ -2,8 +2,10 @@ import { Observable } from 'rxjs';
 import {Component, OnInit} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SelectionModel } from '@angular/cdk/collections';
+import { MatTableDataSource } from '@angular/material/table';
 import { MatSnackBar } from '@angular/material';
 import {
+  Audio,
   DecodeMessage,
   Project,
   Resource,
@@ -25,10 +27,14 @@ export class DecodingUploadComponent implements OnInit {
 
   project$:Observable<Project>;
   training$:Observable<Training>;
-  //resources$:Observable<Array<Resource>>;
-  decodings$:Observable<Array<DecodeMessage>>;
+  audios$:Observable<Array<Audio>>;
 
-  currentDecodingResources:Blob[];
+  currentAudios:Audio[];
+  allAudios:MatTableDataSource<Audio>;
+
+  displayedColumns:string[] = ['select', 'name'];
+  
+  public historySelection = new SelectionModel<Audio>(true, []);
   
   constructor(
     private route: ActivatedRoute,
@@ -39,6 +45,8 @@ export class DecodingUploadComponent implements OnInit {
     private snackBar: MatSnackBar) {}
 
   ngOnInit() {
+    this.currentAudios = [];
+
     this.projectUuid = this.route.snapshot.paramMap.get('uuid');
     this.trainingVersion =  +this.route.snapshot.paramMap.get('id');
 
@@ -46,27 +54,75 @@ export class DecodingUploadComponent implements OnInit {
     this.training$ = this.trainingService.getTrainingByVersion(this.projectUuid, this.trainingVersion);
     this.project$ = this.projectService.getProjectByUuid(this.projectUuid);
     //this.resources$ = this.resourceService.getResource();
-    this.decodings$ = this.decodeService.getDecodings(
-      this.projectUuid,
-      this.trainingVersion);
+    this.audios$ = new Observable<Array<Audio>>();//this.decodeService.getAllAudio();
 
-    this.currentDecodingResources = [];
+    // TODO: get audio files that are added to a decode
+    this.audios$.subscribe(audios => {
+      console.log(audios);
+       this.currentAudios = [];
+       this.allAudios = new MatTableDataSource<Audio>();
+    })
+
   }
 
-  // removes selected history elements
-  remove(selectedResources) {
+/** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.historySelection.selected.length;
+    const numRows = this.allAudios.data.length;
+    return numSelected === numRows;
+  }
 
-    selectedResources.forEach(item => {
-      const resource:Blob = item.value;
-      let index:number = this.currentDecodingResources.findIndex(d => d === resource);
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+        this.historySelection.clear() :
+        this.allAudios.data.forEach(row => this.historySelection.select(row));
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: Resource): string {
+    if (!row) {
+      return "${this.isAllSelected() ? 'select' : 'deselect'} all";
+    }
+
+    return "${this.historySelection.isSelected(row) ? 'deselect' : 'select'} row ${row}";
+  }
+
+  // copies selected history elements to current panel
+  copyAudio() {
+    this.historySelection.selected.forEach(audio => {
+      this.currentAudios.push(audio);
+      console.log("Assgin resource: " + audio.uuid + "Name: " + audio.name + " to Decode");
+      // TODO: change to assignAudioToDecode()
+      /*this.trainingService.assignResourceToTraining(
+        this.projectUuid,
+        this.trainingVersion,
+        { resource_uuid: resource.uuid })
+      .subscribe(this.currentTrainingResources.push);*/
+    });
+
+    this.snackBar.open("Copied audio to decode...", "", { duration: 3000 });
+  }
+
+  // removes selected training resources
+  remove(selectedAudio) {
+
+    selectedAudio.forEach(item => {
+      const audio:Audio = item.value;
+      let index:number = this.currentAudios.findIndex(d => d === audio);
 
       if(index > -1) {
-          console.log("Removed resource: " + this.currentDecodingResources[index] + " from decoding: " + this.projectUuid);
-          this.currentDecodingResources.splice(index, 1);
+        // TODO deleteAssignedResourceFromTraining
+        this.decodeService.deleteAudioByUuid(
+          audio.uuid
+        ).subscribe(r => {
+          console.log("Removed audio: " + r.name + " from decode");
+          this.currentAudios.splice(index, 1);
+        });
       }
     });
 
-    this.snackBar.open("Removed resource from training...", "", { duration: 3000 });
+    this.snackBar.open("Removed audio from decode...", "", { duration: 3000 });
   }
 
   // uploads file and show preview
@@ -76,24 +132,31 @@ export class DecodingUploadComponent implements OnInit {
 
   uploadAudio(file) {
     console.log("Uploaded audio: " + file.files[0].name);
-    const audioFile:Blob = file.files[0] as Blob;
+    const blobFile:Blob = file.files[0] as Blob;
 
-    // FIXME: causes compile error
-    // this.decodeService.startDecode(
-    //   this.projectUuid,
-    //   this.trainingVersion,
-    //   audioFile);
+    this.decodeService.uploadAudio(blobFile)
+      .subscribe(audio => {
+        // TODO assign audio to decode!!!
+    });
+    // creates resource and starts the TextPrepWorker to create the corupus
+    /*this.resourceService.createResource(blobFile)
+      .subscribe(resource => {
+        console.log("Created Resource: " + resource.uuid);
+        this.currentTrainingResources.push(resource);
+        //console.log("Assgin resource: " + resource.uuid + "Name: " + resource.name + " to training: " + this.trainingVersion);
+        this.trainingService.assignResourceToTraining(
+          this.projectUuid,
+          this.trainingVersion,
+          { resource_uuid: resource.uuid })
+        .subscribe(this.currentTrainingResources.push);
+    });*/
 
-    // this.currentDecodingResources.push(audioFile);
-    // this.snackBar.open("Start audio decoding...", "", { duration: 3000 });
-    this.snackBar.open("TODO: Implement/Fix start audio decoding...", "", { duration: 3000 });
+    this.snackBar.open("Added resource to training...", "", { duration: 3000 });
   }
 
   reloadProject() {
     //console.log("Reload values on next..");
     this.project$ = this.projectService.getProjectByUuid(this.projectUuid);
     this.training$ = this.trainingService.getTrainingByVersion(this.projectUuid, this.trainingVersion);
-    this.decodings$ = this.decodeService.getDecodings(this.projectUuid, this.trainingVersion);
-    let decodingResults = this.decodeService.getDecodeResult(this.projectUuid, this.trainingVersion, "")
   }
 }
