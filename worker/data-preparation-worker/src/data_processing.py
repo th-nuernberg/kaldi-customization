0c9abee1f1dbd72b75aea3cd42487737150abaad
Dino Pehratovic <dino.pehratovic@outlook.de>
2019-09-28 14:42:25 +0200

    Data-Prep-Worker will now download the lexicon.txt file of the chosen acoustic model and extend this one with new words and use the new lexicon.txt file for training

--- worker/data-preparation-worker/src/data_processing.py
+++ worker/data-preparation-worker/src/data_processing.py
@@ -4,6 +4,88 @@ import re
 import subprocess
 
 
+def compare_lexicon_with_word_list(final_word_list, lexicon):
+    '''
+    This function takes two arguments. 
+        1. final_word_list which was created by the create_unique_word_list function
+        2. lexicon which was downloaded from the acoustic model bucket.
+    Both parameters are just paths. Therefore, both files need to be opened and read with a handler.
+
+    This function will create a unique word list which will afterwards be used by the 
+    execute_phonetisaurus function.
+
+    In order to create the unique word list, this function will compare both lists. 
+    Therefore, every element of the final_word_list is checked. If an element is already
+    located within the lexicon list, it is ignored.
+
+    That means the unique word list consists of words which are really new for the lexicon file.
+
+    This function returns a list of new words for the lexicon.txt file.
+    '''
+    final_word_list_handler = open(final_word_list, "r")
+    lexicon_handler = open(lexicon, "r")
+
+    word_list = []
+    lexicon_word_list = []
+    unique_word_list = []
+
+    line = lexicon_handler.readline()
+    while line:
+        word = line.split()[0]
+        lexicon_word_list.append(word)
+        line = lexicon_handler.readline()
+
+
+    line = final_word_list_handler.readline()
+    while line:
+        word = line.split()[0]
+        word_list.append(word)
+        line = final_word_list_handler.readline()
+
+
+    for element in word_list:
+        if element in lexicon_word_list:
+            continue
+        else:
+            unique_word_list.append(element)
+
+    return unique_word_list
+
+
+def combine_old_and_new_lexicon_files(old_lexicon, new_lexicon):
+    '''
+    The function takes two parameters:
+        1. old_lexicon which is basically the downloaded lexicon from the acoustic bucket
+        2. new_lexicon which contains the result of the phonetisaurus. Therefore, all words from the unique word list
+           and their phones are located within the new_lexicon 
+    Both parameters contain a path. Therefore, it is necessary to read both files with a handler.
+
+
+    This function will extend the old lexicon file with new words and phones from the new lexicon.
+
+    A list is returned by this function which contains all words, from old_lexicon and new_lexicon.
+    '''
+    old_lexicon_handler = open(old_lexicon, "r")
+    new_lexicon_handler = open(new_lexicon, "r")
+
+    lexicon = [] 
+
+    line = old_lexicon_handler.readline()
+    while line:
+        line = line.split("\n")[0]
+        lexicon.append(line)
+        line = old_lexicon_handler.readline()
+
+    line = new_lexicon_handler.readline()
+    while line:
+        line = line.split("\n")[0]
+        lexicon.append(line)
+        line = new_lexicon_handler.readline()
+    
+    lexicon = sorted(lexicon)
+    return lexicon
+
+
 def execute_phonetisaurus():
     '''
     This function executes the phonetisaurus-apply command
@@ -19,7 +101,7 @@ def execute_phonetisaurus():
     '''
     with open("/data_prep_worker/out/lexicon.txt", "w") as file_handler:            
         # Applies the previously trained graph onto the word list and creates phones for all words
-        subprocess.call(["phonetisaurus-apply", "--model", "/data_prep_worker/in/g2p_model.fst", "--word_list", "/data_prep_worker/out/final_word_list", 
+        subprocess.call(["phonetisaurus-apply", "--model", "/data_prep_worker/in/g2p_model.fst", "--word_list", "/data_prep_worker/out/unique_word_list", 
                          "--beam", "10000", "-g", "-t", "10000"], stdout=file_handler)
 
 
@@ -77,7 +159,6 @@ def merge_corpus_list(corpus_list, log_file_handler):
     print("There is only one element within the given corpus list. Therefore no merge was needed.")
     log_file_handler.write("There is only one element within the given corpus list. Therefore no merge was needed. \n")
     return return_corpus_list
-
 
 
 def remove_local_files(path):
