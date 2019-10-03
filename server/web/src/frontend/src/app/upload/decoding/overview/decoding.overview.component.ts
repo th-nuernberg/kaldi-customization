1b93afb77d28409b90228ef3c294d90a401b6a55
Florian Richter <richter.r.florian@gmail.com>
2019-10-03 12:46:57 +0200

    updated decoding overview page: displays all relevant data (at the moment)

--- server/web/src/frontend/src/app/upload/decoding/overview/decoding.overview.component.ts
+++ server/web/src/frontend/src/app/upload/decoding/overview/decoding.overview.component.ts
@@ -1,8 +1,10 @@
 import { Observable } from 'rxjs';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 import {
+  Audio,
   DecodeSession,
   Project,
   Training,
@@ -31,16 +33,21 @@ export class DecodingOverviewComponent implements OnInit {
 
   decodeSessionLog$:Observable<string>;
 
-  graphUrl;
+  currentlyPlayingAudio? : {
+    audio: Audio,
+    data: string
+  } = null;
+  @ViewChild('audioPlayer') audioPlayer;
 
-  	constructor(
-    	private route: ActivatedRoute,
-      private router: Router,
-      private snackBar: MatSnackBar,
-	    private trainingService: TrainingService,
-	    private loggingService: LoggingService,
-      private projectService: ProjectService,
-      private decodeService: DecodeService
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private sanitizer:DomSanitizer,
+    private snackBar: MatSnackBar,
+    private trainingService: TrainingService,
+    private loggingService: LoggingService,
+    private projectService: ProjectService,
+    private decodeService: DecodeService
 	) { }
 	ngOnInit() {
       this.projectUuid = this.route.snapshot.paramMap.get('puuid');
@@ -50,8 +57,13 @@ export class DecodingOverviewComponent implements OnInit {
     	this.project$ = this.projectService.getProjectByUuid(this.projectUuid)
       this.training$ = this.trainingService.getTrainingByVersion(this.projectUuid, this.trainingVersion);
       this.decodeSession$ = this.decodeService.getDecodeSession(this.projectUuid, this.trainingVersion, this.sessionUuid);
-
       this.decodeSessionLog$ = this.loggingService.getDecodeSessionLog(this.projectUuid, this.trainingVersion, this.sessionUuid);
+
+      /*
+        Play audio
+        copy to clipboard - transcripts
+        download - transcripts
+      */
 	}
 
 	ngOnDestroy() {	}
@@ -77,5 +89,78 @@ export class DecodingOverviewComponent implements OnInit {
     a.dataset.downloadurl = ['text/plain', a.download, a.href].join(':');
     event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
     a.dispatchEvent(event);
+  }
+
+  downloadTranscript(data, name:string) {
+    let fileName = name.split('.').slice(0, -1).join('.');
+    this.snackBar.open("Lade" + fileName + " Transkript herunter...", "", AppConstants.snackBarConfig);
+
+    if(!data) {
+      console.error("No data!");
+      return;
+    }
+
+    let blob = new Blob([data], {type: 'text/plain'});
+    let event = document.createEvent('MouseEvent');
+    let a = document.createElement('a');
+
+    a.href = URL.createObjectURL(blob);
+    a.download = fileName + '.txt';
+    a.dataset.downloadurl = ['text/plain', a.download, a.href].join(':');
+    event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    a.dispatchEvent(event);
+  }
+
+  copyToClipboard(text) {
+    let tempTextArea = document.createElement('textarea');
+    tempTextArea.style.position = 'fixed';
+    tempTextArea.style.left = '0';
+    tempTextArea.style.top = '0';
+    tempTextArea.style.opacity = '0';
+    tempTextArea.value = text;
+
+    document.body.appendChild(tempTextArea);
+    tempTextArea.select();
+    tempTextArea.focus();
+    document.execCommand('copy');
+    document.body.removeChild(tempTextArea);
+  }
+
+  audioData() {
+    if (!this.currentlyPlayingAudio)
+      return null;
+
+    return this.sanitizer.bypassSecurityTrustResourceUrl(this.currentlyPlayingAudio.data);
+  }
+
+  triggerAudio(event, audio) {
+    event.stopPropagation();
+
+    if (this.isPlaying(audio)) {
+      this.currentlyPlayingAudio = null;
+    } else {
+      this.decodeService.getAudioData(audio.uuid)
+        .subscribe(data => {
+          const audioData = URL.createObjectURL(data);
+          if (this.currentlyPlayingAudio) {
+            URL.revokeObjectURL(this.currentlyPlayingAudio.data);
+          }
+
+          this.currentlyPlayingAudio = {
+            audio: audio,
+            data: audioData
+          };
+
+          setTimeout(() => this.audioPlayer.nativeElement.play(), 0);
+        });
+      }
+  }
+
+  stopAudio() {
+    this.currentlyPlayingAudio = null;
+  }
+
+  isPlaying(audio: Audio) {
+    return (this.currentlyPlayingAudio && this.currentlyPlayingAudio.audio.uuid == audio.uuid);
   }
 }
