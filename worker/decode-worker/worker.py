4d20cfeb3105466dafd86743e36e9cce68122ab3
Dino Pehratovic <dino.pehratovic@outlook.de>
2019-10-09 22:17:37 +0200

    Added logging for the kaldi- and decode-workers. Updated logging for the data-preparation-worker. Updated DecodeTask class, because it contained a deprecated property!

--- worker/decode-worker/worker.py
+++ worker/decode-worker/worker.py
@@ -26,79 +26,169 @@ os.makedirs(acoustic_model_folder)
 if os.path.exists(workspace_path):
     shutil.rmtree(workspace_path)
 
+
+def finish_logging(log_file_handler, minio_client, task):
+    log_file_handler.close()
+    logfile_result = upload_to_bucket(minio_client, minio_buckets["LOG_BUCKET"], "decode_worker/{}/{}".format(task.training_id, "log.txt"), "/log.txt")
+    if not logfile_result[0]:
+        print("An error occurred during the upload of the logfile.")
+    print("Logfile was successfully uploaded")
+
+
 if __name__ == "__main__":
     try:
         conf, tasks, status, minio_client = parse_args('Decode Worker Connector', task_queue=redis_config.redis_queues["DECODING_QUEUE"])
 
         for task in tasks.listen():
-            print("Read task from queue:")
+            print("Received the following task from Decode-Queue:")
             print(task)
+
+            task = DecodeTask(**task)
 
             acoustic_model_id = str(task["acoustic_model_id"])
             audioresource_uuids = task["audio_uuids"]
             training_id = str(task["training_id"])
             decode_uuid = task["decode_uuid"]
 
+            log_file_handler = open("/log.txt", "w")
+            log_file_handler.write("Starting to process the received task \n")
+            log_file_handler.write("{}\n".format(task))
+
             if not os.path.exists(acoustic_model_folder):
-                        os.makedirs(workspace_path)
+                os.makedirs(workspace_path)
+                log_file_handler.write("Needed acoustic model directory did not exist. Therefore, it was created! \n")
+            log_file_handler.write("Needed acoustic model directory exists. Processing continues! \n")
+
 
             # cache acoustic models when used once and reduce download
-            cur_acoustic_model_path = os.path.join(acoustic_model_folder,str(acoustic_model_id))
-            ivector_extractor_path = os.path.join(cur_acoustic_model_path,"extractor")
-            if(acoustic_model_id not in downloaded_acoustic_models):
-                os.makedirs(cur_acoustic_model_path)
-                download_from_bucket(minio_client, acoustic_model_bucket, acoustic_model_id + "/final.mdl", os.path.join(cur_acoustic_model_path,"final.mdl"))
-                download_from_bucket(minio_client, acoustic_model_bucket, acoustic_model_id + "/tree", os.path.join(cur_acoustic_model_path,"tree"))
-                download_from_bucket(minio_client, acoustic_model_bucket, acoustic_model_id + "/cmvn_opts", os.path.join(cur_acoustic_model_path,"cmvn_opts"))
+            try:
+                log_file_handler.write("Trying to download all needed acoustic model files! \n")
+                cur_acoustic_model_path = os.path.join(acoustic_model_folder,str(acoustic_model_id))
+                ivector_extractor_path = os.path.join(cur_acoustic_model_path,"extractor")
+                if(acoustic_model_id not in downloaded_acoustic_models):
+                    os.makedirs(cur_acoustic_model_path)
+                    download_from_bucket(minio_client, acoustic_model_bucket, acoustic_model_id + "/final.mdl", os.path.join(cur_acoustic_model_path,"final.mdl"))
+                    download_from_bucket(minio_client, acoustic_model_bucket, acoustic_model_id + "/tree", os.path.join(cur_acoustic_model_path,"tree"))
+                    download_from_bucket(minio_client, acoustic_model_bucket, acoustic_model_id + "/cmvn_opts", os.path.join(cur_acoustic_model_path,"cmvn_opts"))
 
-                os.makedirs(ivector_extractor_path)
-                download_from_bucket(minio_client, acoustic_model_bucket, acoustic_model_id + "/extractor/final.dubm", os.path.join(cur_acoustic_model_path,"extractor/final.dubm"))
-                download_from_bucket(minio_client, acoustic_model_bucket, acoustic_model_id + "/extractor/final.ie", os.path.join(cur_acoustic_model_path,"extractor/final.ie"))
-                download_from_bucket(minio_client, acoustic_model_bucket, acoustic_model_id + "/extractor/final.ie.id", os.path.join(cur_acoustic_model_path,"extractor/final.ie.id"))
-                download_from_bucket(minio_client, acoustic_model_bucket, acoustic_model_id + "/extractor/final.mat", os.path.join(cur_acoustic_model_path,"extractor/final.mat"))
-                download_from_bucket(minio_client, acoustic_model_bucket, acoustic_model_id + "/extractor/global_cmvn.stats", os.path.join(cur_acoustic_model_path,"extractor/global_cmvn.stats"))
-                download_from_bucket(minio_client, acoustic_model_bucket, acoustic_model_id + "/extractor/online_cmvn.conf", os.path.join(cur_acoustic_model_path,"extractor/online_cmvn.conf"))
-                download_from_bucket(minio_client, acoustic_model_bucket, acoustic_model_id + "/extractor/splice_opts", os.path.join(cur_acoustic_model_path,"extractor/splice_opts"))
+                    os.makedirs(ivector_extractor_path)
+                    download_from_bucket(minio_client, acoustic_model_bucket, acoustic_model_id + "/extractor/final.dubm", os.path.join(cur_acoustic_model_path,"extractor/final.dubm"))
+                    download_from_bucket(minio_client, acoustic_model_bucket, acoustic_model_id + "/extractor/final.ie", os.path.join(cur_acoustic_model_path,"extractor/final.ie"))
+                    download_from_bucket(minio_client, acoustic_model_bucket, acoustic_model_id + "/extractor/final.ie.id", os.path.join(cur_acoustic_model_path,"extractor/final.ie.id"))
+                    download_from_bucket(minio_client, acoustic_model_bucket, acoustic_model_id + "/extractor/final.mat", os.path.join(cur_acoustic_model_path,"extractor/final.mat"))
+                    download_from_bucket(minio_client, acoustic_model_bucket, acoustic_model_id + "/extractor/global_cmvn.stats", os.path.join(cur_acoustic_model_path,"extractor/global_cmvn.stats"))
+                    download_from_bucket(minio_client, acoustic_model_bucket, acoustic_model_id + "/extractor/online_cmvn.conf", os.path.join(cur_acoustic_model_path,"extractor/online_cmvn.conf"))
+                    download_from_bucket(minio_client, acoustic_model_bucket, acoustic_model_id + "/extractor/splice_opts", os.path.join(cur_acoustic_model_path,"extractor/splice_opts"))
 
-                downloaded_acoustic_models.add(acoustic_model_id)
-            
+                    downloaded_acoustic_models.add(acoustic_model_id)
+            except Exception as e:
+                print(e)
+                log_file_handler.write("While trying to download all needed acoustic model files, the following error occurred: \n")
+                log_file_handler.write("############################################################################# \n")
+                log_file_handler.write("At least one download failed! Therefore, the task failed too!\n")
+
+                finish_logging(log_file_handler, minio_client, task)
+
+                status.submit(DecodeStatus(id=DecodeStatusCode.FAILURE, decode_uuid=decode_uuid, transcripts=result))
+            log_file_handler.write("Successfully downloaded all needed acoustic model files. Processing continues! \n")
+
             # Download Graph files from training
-            cur_graph_path = os.path.join(workspace_path,"graph")
-            download_graph_path = os.path.join(workspace_path, "graph.zip")
-            download_from_bucket(minio_client, training_bucket, training_id + "/graph.zip", download_graph_path)
-            shutil.unpack_archive(download_graph_path, cur_graph_path)
+            try:
+                log_file_handler.write("Trying to download graph archive which is needed for decoding! \n")
+                cur_graph_path = os.path.join(workspace_path,"graph")
+                download_graph_path = os.path.join(workspace_path, "graph.zip")
+                download_from_bucket(minio_client, training_bucket, training_id + "/graph.zip", download_graph_path)
+                shutil.unpack_archive(download_graph_path, cur_graph_path)
+            except Exception as e:
+                print(e)
+                log_file_handler.write("While trying to download the graph archive, the following error occurred: \n")
+                log_file_handler.write("############################################################################# \n")
+                log_file_handler.write("It was not possible to download the needed graph archive. Therefore, the task failed!\n")
+
+                finish_logging(log_file_handler, minio_client, task)
+
+                status.submit(DecodeStatus(id=DecodeStatusCode.FAILURE, decode_uuid=decode_uuid, transcripts=result))
+            log_file_handler.write("Successfully downloaded graph archive. Processing continues! \n")
 
 
             # Download wav files to decode
-            for uuid in audioresource_uuids:
-                download_from_bucket(minio_client, decode_bucket, str(uuid) , os.path.join(workspace_path, str(uuid) + ".wav"))
+            try:
+                log_file_handler.write("Trying to download all audio files which need to be decoded! \n")
+                for uuid in audioresource_uuids:
+                    download_from_bucket(minio_client, decode_bucket, str(uuid) , os.path.join(workspace_path, str(uuid) + ".wav"))
+            except Exception as e:
+                print(e)
+                log_file_handler.write("While trying to download all needed audio files, the following error occurred: \n")
+                log_file_handler.write("############################################################################# \n")
+                log_file_handler.write("At least one download failed. Therefore, this task failed too! \n")
+
+                finish_logging(log_file_handler, minio_client, task)
+
+                status.submit(DecodeStatus(id=DecodeStatusCode.FAILURE, decode_uuid=decode_uuid, transcripts=result))
+            log_file_handler.write("Successfully downloaded all audio files. Processing continues! \n")
 
             # create data dir
-            data_path = os.path.join(workspace_path,"data")
-            os.makedirs(data_path)
-            with open(os.path.join(data_path,"utt2spk"),"w") as f:
-                for i,uuid in enumerate(audioresource_uuids):
-                    f.write("{} generic_speaker{}".format(uuid,i))
-            
-            with open(os.path.join(data_path,"wav.scp"),"w") as f:
-                for uuid in audioresource_uuids:
-                    f.write("{} {}".format(uuid,os.path.join(workspace_path, str(uuid) + ".wav")))
+            try:
+                log_file_handler.write("Trying to create utt2spk and wav.scp files for the decode task! \n")
+                data_path = os.path.join(workspace_path,"data")
+                os.makedirs(data_path)
+                with open(os.path.join(data_path,"utt2spk"),"w") as f:
+                    for i,uuid in enumerate(audioresource_uuids):
+                        f.write("{} generic_speaker{}".format(uuid,i))
+
+                with open(os.path.join(data_path,"wav.scp"),"w") as f:
+                    for uuid in audioresource_uuids:
+                        f.write("{} {}".format(uuid,os.path.join(workspace_path, str(uuid) + ".wav")))
+            except Exception as e:
+                print(e)
+                log_file_handler.write("While trying to create the utt2spk and wav.scp files, the following error occurred: \n")
+                log_file_handler.write("############################################################################# \n")
+                log_file_handler.write("At least one file was not created!\n")
+
+                finish_logging(log_file_handler, minio_client, task)
+
+                status.submit(DecodeStatus(id=DecodeStatusCode.FAILURE, decode_uuid=decode_uuid, transcripts=result))
+            log_file_handler.write("Successfully created utt2spk and wav.scp files! Processing continues! \n")
 
             # decode
-            os.chdir("/kaldi/scripts/")
-            subprocess.call("/kaldi/scripts/decode.sh"+ " {} {} {}".format(workspace_path, cur_acoustic_model_path, cur_graph_path),shell=True)
-            os.chdir("/")
-            decode_path = os.path.join(cur_acoustic_model_path,"decode")
+            try:
+                log_file_handler.write("Decoding of the downloaded audio files starts! \n")
+                os.chdir("/kaldi/scripts/")
+                subprocess.call("/kaldi/scripts/decode.sh"+ " {} {} {}".format(workspace_path, cur_acoustic_model_path, cur_graph_path),shell=True)
+                os.chdir("/")
+                decode_path = os.path.join(cur_acoustic_model_path,"decode")
+            except Exception as e:
+                print(e)
+                log_file_handler.write("During the decoding process an error has occurred which leads to a failing task! \n")
 
-            result = dict()
-            # extract from acoustic_model/decode
-            with open(os.path.join(decode_path, "log", "decode.1.log")) as f:
-                for line in f:
-                    line = line.split()
-                    if line[0] in audioresource_uuids:
-                        result[line[0]]=[" ".join(line[1:])]
+                finish_logging(log_file_handler, minio_client, task)
 
-            print("FOUND RESULTS:\n"+str(result))
+                status.submit(DecodeStatus(id=DecodeStatusCode.FAILURE, decode_uuid=decode_uuid, transcripts=result))
+            log_file_handler.write("Successfully decoded all audio files! \n")
+
+            try:
+                log_file_handler.write("Final step: Retrieve decoding transcripts! \n")
+                result = dict()
+                # extract from acoustic_model/decode
+                with open(os.path.join(decode_path, "log", "decode.1.log")) as f:
+                    for line in f:
+                        line = line.split()
+                        if line[0] in audioresource_uuids:
+                            result[line[0]]=[" ".join(line[1:])]
+
+                print("FOUND RESULTS:\n"+str(result))
+            except Exception as e:
+                print(e)
+                log_file_handler.write("While trying to retrieve the decoding transcript, the following error occurred: \n")
+                log_file_handler.write("############################################################################# \n")
+                log_file_handler.write("It was not possible to access the requested file, or no transcript was found! \n")
+
+                finish_logging(log_file_handler, minio_client, task)
+
+                status.submit(DecodeStatus(id=DecodeStatusCode.FAILURE, decode_uuid=decode_uuid, transcripts=result))
+            log_file_handler.write("Successfully retrieved the decoding transcripts!\n")
+
+            finish_logging(log_file_handler, minio_client, task)
 
             # unload resources
             shutil.rmtree(workspace_path)
--- worker/kaldi-worker/worker.py
+++ worker/kaldi-worker/worker.py
@@ -1,12 +1,13 @@
 ﻿#!/usr/bin/python
-from connector import *
 import os
 import shutil
-
-import subprocess
-from minio_communication import download_from_bucket, upload_to_bucket, minio_buckets
 import redis_config
 import json
+import subprocess
+
+from connector import *
+from minio_communication import download_from_bucket, upload_to_bucket, minio_buckets
+
 
 acoustic_model_bucket = minio_buckets['ACOUSTIC_MODELS_BUCKET']
 training_bucket = minio_buckets["TRAINING_BUCKET"]
@@ -24,54 +25,124 @@ if os.path.exists(acoustic_model_folder):
     shutil.rmtree(acoustic_model_folder)
 os.makedirs(acoustic_model_folder)
 
+
+
+def finish_logging(log_file_handler, minio_client, task):
+    log_file_handler.close()
+    logfile_result = upload_to_bucket(minio_client, minio_buckets["LOG_BUCKET"], "kaldi_worker/{}/{}".format(task.training_id, "log.txt"), "/log.txt")
+    if not logfile_result[0]:
+        print("An error occurred during the upload of the logfile.")
+    print("Logfile was successfully uploaded")
+
+
 if __name__ == "__main__":
     try:
-        conf, tasks, status, minio_client = parse_args(
+        conf, tasks, status_queue, minio_client = parse_args(
             'Kaldi Worker Connector', task_queue=redis_config.redis_queues["KALDI_QUEUE"])
 
         for task in tasks.listen():
-            print("Read task from queue:")
+            print("Received the following task from Kaldi-Queue:")
             print(task)
 
             task = KaldiTask(**task)
 
+            log_file_handler = open("/log.txt", "w")
+            log_file_handler.write("Starting to process the received task \n")
+            log_file_handler.write("{}\n".format(task))
+
             if not os.path.exists(acoustic_model_folder):
                 os.makedirs(workspace_path)
+                log_file_handler.write("Workspace directory did not exist. Therefore, the needed directory was created. \n")
+            log_file_handler.write("Workspace directory already exists. Processing continues. \n")
+
 
             # cache models when used once and reduce download
-            cur_acoustic_model_path = os.path.join(acoustic_model_folder, str(task.acoustic_model_id))
-            phone_symbol_table = os.path.join(cur_acoustic_model_path,"phones.txt")
-            if task.acoustic_model_id not in downloaded_acoustic_models:
-                os.makedirs(cur_acoustic_model_path)
-                download_from_bucket(minio_client, acoustic_model_bucket, "{}/final.mdl".format(task.acoustic_model_id), os.path.join(cur_acoustic_model_path, "final.mdl"))
-                download_from_bucket(minio_client, acoustic_model_bucket, "{}/tree".format(task.acoustic_model_id), os.path.join(cur_acoustic_model_path, "tree"))
-                download_from_bucket(minio_client, acoustic_model_bucket, "{}/phones.txt".format(task.acoustic_model_id), phone_symbol_table)
+            try:
+                log_file_handler.write("Starting to download all needed files from the corresponding MinIO-bucket! \n")
+                cur_acoustic_model_path = os.path.join(acoustic_model_folder, str(task.acoustic_model_id))
+                phone_symbol_table = os.path.join(cur_acoustic_model_path,"phones.txt")
+                if task.acoustic_model_id not in downloaded_acoustic_models:
+                    os.makedirs(cur_acoustic_model_path)
+                    download_from_bucket(minio_client, acoustic_model_bucket, "{}/final.mdl".format(task.acoustic_model_id), os.path.join(cur_acoustic_model_path, "final.mdl"))
+                    download_from_bucket(minio_client, acoustic_model_bucket, "{}/tree".format(task.acoustic_model_id), os.path.join(cur_acoustic_model_path, "tree"))
+                    download_from_bucket(minio_client, acoustic_model_bucket, "{}/phones.txt".format(task.acoustic_model_id), phone_symbol_table)
 
-                downloaded_acoustic_models.add(task.acoustic_model_id)
+                    downloaded_acoustic_models.add(task.acoustic_model_id)
 
-            # load resources
-            download_from_bucket(minio_client, training_bucket, "{}/lexicon.txt".format(task.training_id), lexicon_path)
-            download_from_bucket(minio_client, training_bucket, "{}/corpus.txt".format(task.training_id), corpus_path)
+                # load resources
+                download_from_bucket(minio_client, training_bucket, "{}/lexicon.txt".format(task.training_id), lexicon_path)
+                download_from_bucket(minio_client, training_bucket, "{}/corpus.txt".format(task.training_id), corpus_path)
+            except Exception as e:
+                print(e)
+                log_file_handler.write("While trying to download all needed files for this worker, the following error occurred: \n")
+                log_file_handler.write("############################################################################# \n")
+                log_file_handler.write("At least one download failed! It is not possible to retrieve all needed files. Therefore, the task fails!\n")
+
+                finish_logging(log_file_handler=log_file_handler, minio_client=minio_client, task=task)
+
+                status_queue.submit(KaldiStatus(id=KaldiStatusCode.FAILURE, training_id=task.training_id))
+            log_file_handler.write("Successfully downloaded all needed files! \n")
 
             # train resources
-            new_graph_dir = os.path.join(script_root_path, "new_graph")
-            os.chdir("/kaldi/scripts/")
-            subprocess.call("/kaldi/scripts/create_new_graph.sh {} {} {} {} {} {}".format(lexicon_path, corpus_path,
+            try:
+                log_file_handler.write("Processing continues by creating a new graph with the given training resources! \n")
+                new_graph_dir = os.path.join(script_root_path, "new_graph")
+                os.chdir("/kaldi/scripts/")
+                subprocess.call("/kaldi/scripts/create_new_graph.sh {} {} {} {} {} {}".format(lexicon_path, corpus_path,
                                                                                           cur_acoustic_model_path, new_graph_dir, workspace_path, phone_symbol_table), shell=True)
-            os.chdir("/")
+                os.chdir("/")
+            except Exception as e:
+                print(e)
+                log_file_handler.write("While trying to create the new graph an error occurred. \n")
+                log_file_handler.write("Therefore, it was not possible to create a new graph for this training. That means this task failed! \n")
+
+                finish_logging(log_file_handler=log_file_handler, minio_client=minio_client, task=task)
+
+                status_queue.submit(KaldiStatus(id=KaldiStatusCode.FAILURE, training_id=task.training_id))
+            log_file_handler.write("Successfully created a new graph with the given training resources!\n")
 
             # Upload new model
-            new_graph_archive = os.path.join(script_root_path, "graph")
-            archive_format = "zip"
-            shutil.make_archive(base_name=new_graph_archive,
-                                format=archive_format, root_dir=new_graph_dir, base_dir="./")
-            upload_to_bucket(minio_client, training_bucket, "{}/graph.zip".format(task.training_id), new_graph_archive + "." + archive_format)
+            try:
+                log_file_handler.write("Starting to compress all files which will be uploaded!\n")
+                new_graph_archive = os.path.join(script_root_path, "graph")
+                archive_format = "zip"
+                shutil.make_archive(base_name=new_graph_archive,
+                                    format=archive_format, root_dir=new_graph_dir, base_dir="./")
+            except Exception as e:
+                print(e)
+
+                log_file_handler.write("While trying to compress all needed files, the following error occurred: \n")
+                log_file_handler.write("############################################################################# \n")
+                log_file_handler.write("Compressing failed! Task failed! \n")
+
+                finish_logging(log_file_handler=log_file_handler, minio_client=minio_client, task=task)
+
+                status_queue.submit(KaldiStatus(id=KaldiStatusCode.FAILURE, training_id=task.training_id))
+            log_file_handler.write("Successfully compressed files. Ready for upload! \n")
+
+            try:
+                log_file_handler.write("Final step: Upload of the ZIP archive into its corresponding MinIO-bucket! \n")
+                upload_to_bucket(minio_client, training_bucket, "{}/graph.zip".format(task.training_id), new_graph_archive + "." + archive_format)
+            except Exception as e:
+                print(e)
+
+                log_file_handler.write("While trying to upload the ZIP archive, the following error occurred: \n")
+                log_file_handler.write("############################################################################# \n")
+                log_file_handler.write("The upload failed! Therefore, the task failed too! \n")
+
+                finish_logging(log_file_handler=log_file_handler, minio_client=minio_client, task=task)
+                status_queue.submit(KaldiStatus(id=KaldiStatusCode.FAILURE, training_id=task.training_id))
+
+            log_file_handler.write("Successfully uploaded the ZIP archive! \n")
 
             # unload resources
             shutil.rmtree(workspace_path)
 
+            # uploading created log-file
+            finish_logging(log_file_handler=log_file_handler, minio_client=minio_client, task=task)
+
             # write in status queue
-            status.submit(KaldiStatus(id=KaldiStatusCode.SUCCESS, training_id=task.training_id))
+            status_queue.submit(KaldiStatus(id=KaldiStatusCode.SUCCESS, training_id=task.training_id))
 
     except KeyboardInterrupt:
         shutil.rmtree(acoustic_model_folder)
